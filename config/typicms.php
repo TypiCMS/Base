847817aa8cc83de51c879eb148a50942b0cf8699
Samuel De Backer <sdebacker@gmail.com>
2015-02-10 11:21:58 +0100

    config

--- config/typicms.php
+++ config/typicms.php
@@ -13,4 +13,20 @@ return [
         'per_page' => 50,
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Modules that can be linked to a menu item
+    |--------------------------------------------------------------------------
+    */
+    'modules_for_menu_items' => [
+        'Categories',
+        'Events',
+        'Galleries',
+        'News',
+        'Partners',
+        'Places',
+        'Projects',
+        'Tags',
+    ],
+
 ];
