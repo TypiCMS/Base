c0c671eac4e0340fa760ef0864b572af3a7495e1
Samuel De Backer <sdebacker@gmail.com>
2015-02-22 12:04:20 +0100

    setup for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,18 +2,34 @@
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
-	/**
-	 * Creates the application.
-	 *
-	 * @return \Illuminate\Foundation\Application
-	 */
-	public function createApplication()
-	{
-		$app = require __DIR__.'/../bootstrap/app.php';
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
 
-		$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
 
-		return $app;
-	}
+        return $app;
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->app['path.base'] = __DIR__ . '/..';
+        $this->app['config']->set('database.default', 'sqlite');
+        $this->app['config']->set('database.connections.sqlite', array(
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ));
+        $this->app['config']->set('translatable.locales', ['en', 'fr']);
+        Artisan::call('migrate');
+        $this->seed();
+    }
 
 }
