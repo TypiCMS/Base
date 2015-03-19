0501819807e937070455d062c9654f545931f1c4
Samuel De Backer <sdebacker@gmail.com>
2015-03-20 00:38:37 +0100

    buttons

--- resources/assets/typicms/components/list/directives/typiBtnDelete.js
+++ resources/assets/typicms/components/list/directives/typiBtnDelete.js
@@ -3,9 +3,9 @@ angular.module('typicms').directive('typiBtnDelete', function() {
         scope: {
             action: '&'
         },
-        template: '<button ng-click="action()" class="btn btn-xs btn-link">' +
+        template: '<div ng-click="action()" class="btn btn-xs btn-link">' +
                 '<span class="fa fa-remove"></span>' +
                 '<span class="sr-only" translate>Delete</span>' +
-            '</button>'
+            '</div>'
     };
 });
--- resources/assets/typicms/components/list/directives/typiBtnStatus.js
+++ resources/assets/typicms/components/list/directives/typiBtnStatus.js
@@ -4,10 +4,10 @@ angular.module('typicms').directive('typiBtnStatus', function() {
             model: '=',
             action: '&'
         },
-        template: '<button class="btn btn-xs btn-link" ng-click="action()">' +
+        template: '<div class="btn btn-xs btn-link" ng-click="action()">' +
                 '<span class="fa switch" ng-class="model.status == \'1\' ? \'fa-toggle-on\' : \'fa-toggle-off\'"></span>' +
                 '<span class="sr-only" ng-show="model.status == \'1\'" translate>Online</span>' +
                 '<span class="sr-only" ng-hide="model.status == \'0\'" translate>Offline</span>' +
-            '</button>'
+            '</div>'
     };
 });
