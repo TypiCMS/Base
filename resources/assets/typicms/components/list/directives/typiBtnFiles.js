f9edaaeb587724fb26130a0f7796e002fca3a230
Samuel De Backer <sdebacker@gmail.com>
2015-03-19 21:16:43 +0100

    directives

--- resources/assets/typicms/components/list/directives/typiBtnFiles.js
+++ resources/assets/typicms/components/list/directives/typiBtnFiles.js
@@ -1,5 +1,8 @@
 angular.module('typicms').directive('typiBtnFiles', function() {
     return {
-        templateUrl: '/views/partials/btnFiles.html'
+        scope: {
+            model: '='
+        },
+        template: '<a class="label label-default" ng-class="model.files_count ? \'label-success\' : \'\'" href="/admin/galleries/{{ model.id }}/edit?tab=tab-files">{{ model.files_count }}</a>'
     };
 });
--- resources/assets/typicms/components/list/directives/typiPagination.js
+++ resources/assets/typicms/components/list/directives/typiPagination.js
@@ -1,5 +1,5 @@
 angular.module('typicms').directive('typiPagination', function() {
     return {
-        template: '<div st-pagination="" st-items-by-page="5" st-displayed-pages="9999"></div>'
+        template: '<div st-pagination="" st-items-by-page="itemsByPage" st-displayed-pages="9999"></div>'
     };
 });
