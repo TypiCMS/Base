950b086ff727cf58aee59d7510eaf22c5c72bacd
Samuel De Backer <sdebacker@gmail.com>
2015-04-20 10:55:24 +0200

    CKEditor autocorrect

--- public/components/ckeditor/config.js
+++ public/components/ckeditor/config.js
@@ -28,7 +28,7 @@ CKEDITOR.editorConfig = function( config ) {
     // Set the most common block elements.
     config.format_tags = 'p;h1;h2;h3;h4;h5;h6;pre';
 
-    config.extraPlugins = 'image2,codemirror,clipboard,panelbutton,oembed,justify,quicktable,showblocks';
+    config.extraPlugins = 'image2,codemirror,autocorrect,clipboard,panelbutton,oembed,justify,quicktable,showblocks';
     config.removePlugins = 'image';
 
     // Skin / UI
