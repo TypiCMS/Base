c151d813db9204ced04e926e421a63898e9b1723
Samuel De Backer <sdebacker@gmail.com>
2015-06-08 13:24:19 +0200

    new syntax

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -12,12 +12,12 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        'Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode',
-        'Illuminate\Cookie\Middleware\EncryptCookies',
-        'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
-        'Illuminate\Session\Middleware\StartSession',
-        'Illuminate\View\Middleware\ShareErrorsFromSession',
-        'App\Http\Middleware\VerifyCsrfToken',
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \Illuminate\Cookie\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\VerifyCsrfToken::class,
     ];
 
     /**
@@ -26,12 +26,12 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth.basic'   => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
-        'guest'        => 'App\Http\Middleware\RedirectIfAuthenticated',
-        'admin'        => 'TypiCMS\Modules\Core\Http\Middleware\Admin',
-        'auth'         => 'TypiCMS\Modules\Core\Http\Middleware\Authenticate',
-        'publicAccess' => 'TypiCMS\Modules\Core\Http\Middleware\PublicAccess',
-        'publicLocale' => 'TypiCMS\Modules\Core\Http\Middleware\PublicLocale',
-        'registration' => 'TypiCMS\Modules\Core\Http\Middleware\Registration',
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'admin' => \TypiCMS\Modules\Core\Http\Middleware\Admin::class,
+        'auth' => \TypiCMS\Modules\Core\Http\Middleware\Authenticate::class,
+        'publicAccess' => \TypiCMS\Modules\Core\Http\Middleware\PublicAccess::class,
+        'publicLocale' => \TypiCMS\Modules\Core\Http\Middleware\PublicLocale::class,
+        'registration' => \TypiCMS\Modules\Core\Http\Middleware\Registration::class,
     ];
 }
