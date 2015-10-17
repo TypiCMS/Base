ba3b496590546f984d486e5ebe3f1463249b2064
Samuel De Backer <sdebacker@gmail.com>
2015-10-17 16:20:47 +0200

    PublicLocale middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         \App\Http\Middleware\VerifyCsrfToken::class,
+        \TypiCMS\Modules\Core\Http\Middleware\PublicLocale::class,
     ];
 
     /**
@@ -32,7 +33,6 @@ class Kernel extends HttpKernel
         'auth' => \TypiCMS\Modules\Core\Http\Middleware\Authenticate::class,
         'publicAccess' => \TypiCMS\Modules\Core\Http\Middleware\PublicAccess::class,
         'publicCache' => \TypiCMS\Modules\Core\Http\Middleware\PublicCache::class,
-        'publicLocale' => \TypiCMS\Modules\Core\Http\Middleware\PublicLocale::class,
         'registration' => \TypiCMS\Modules\Core\Http\Middleware\Registration::class,
     ];
 }
