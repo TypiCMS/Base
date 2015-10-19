b0831c308155d4a72222f944ef38f1770a756747
Samuel De Backer <sdebacker@gmail.com>
2015-10-19 17:46:46 +0200

    Notification Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,9 +17,9 @@ class Kernel extends HttpKernel
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \Krucas\Notification\Middleware\NotificationMiddleware::class,
         \App\Http\Middleware\VerifyCsrfToken::class,
         \TypiCMS\Modules\Core\Http\Middleware\PublicLocale::class,
+        \Krucas\Notification\Middleware\NotificationMiddleware::class,
     ];
 
     /**
