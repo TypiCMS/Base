676003e2bc20b46507f6e2f72093d7aadea46066
Samuel De Backer <sdebacker@gmail.com>
2015-10-19 23:51:17 +0200

    publicLocale in route middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,6 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         \App\Http\Middleware\VerifyCsrfToken::class,
-        \TypiCMS\Modules\Core\Http\Middleware\PublicLocale::class,
         \Krucas\Notification\Middleware\NotificationMiddleware::class,
     ];
 
@@ -28,12 +27,13 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth.basic'   => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'guest'        => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'admin'        => \TypiCMS\Modules\Core\Http\Middleware\Admin::class,
-        'auth'         => \TypiCMS\Modules\Core\Http\Middleware\Authenticate::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'admin' => \TypiCMS\Modules\Core\Http\Middleware\Admin::class,
+        'auth' => \TypiCMS\Modules\Core\Http\Middleware\Authenticate::class,
         'publicAccess' => \TypiCMS\Modules\Core\Http\Middleware\PublicAccess::class,
-        'publicCache'  => \TypiCMS\Modules\Core\Http\Middleware\PublicCache::class,
+        'publicLocale' => \TypiCMS\Modules\Core\Http\Middleware\PublicLocale::class,
+        'publicCache' => \TypiCMS\Modules\Core\Http\Middleware\PublicCache::class,
         'registration' => \TypiCMS\Modules\Core\Http\Middleware\Registration::class,
     ];
 }
