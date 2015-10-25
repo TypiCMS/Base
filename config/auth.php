b8bd9093ef4357db63f0da75b83f122658886620
Samuel De Backer <sdebacker@gmail.com>
2015-10-25 14:15:43 +0100

    emails.password view

--- config/auth.php
+++ config/auth.php
@@ -59,7 +59,7 @@ return [
     */
 
     'password' => [
-        'email'  => 'emails.password',
+        'email'  => 'users::emails.password',
         'table'  => 'password_resets',
         'expire' => 60,
     ],
