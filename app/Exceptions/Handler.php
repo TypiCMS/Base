a690a88728245f4be8a8259ec3125ef2772ca3f3
Samuel De Backer <sdebacker@gmail.com>
2015-10-29 09:44:50 +0100

    don't report ModelNotFoundException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -17,6 +17,7 @@ class Handler extends ExceptionHandler
      */
     protected $dontReport = [
         \Symfony\Component\HttpKernel\Exception\HttpException::class,
+        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
     ];
 
     /**
