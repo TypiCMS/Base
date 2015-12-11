0e18148dd85bafe19bb84e78c344b0709e30c933
Samuel De Backer <sdebacker@gmail.com>
2015-12-11 10:07:45 +0100

    config

--- config/javascript.php
+++ config/javascript.php
@@ -11,7 +11,7 @@ return [
     | you want to prepend all JavaScript variables to.
     |
     */
-    'bind_js_vars_to_this_view' => 'core::admin._footer',
+    'bind_js_vars_to_this_view' => 'core::admin.master',
 
     /*
     |--------------------------------------------------------------------------
