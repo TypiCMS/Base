bd803ba84aa10019d5ef70592029efa1ec30e216
Samuel De Backer <sdebacker@gmail.com>
2015-12-24 15:14:44 +0100

    default routes.php file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,3 +10,22 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+// Route::group(['middleware' => ['web']], function () {
+//
+// });
