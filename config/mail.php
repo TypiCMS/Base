8b6f706b1f2b3097034f85ff3efcafdda8f55861
Samuel De Backer <sdebacker@gmail.com>
2015-12-27 19:06:59 +0100

    default from email

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'info@example.com', 'name' => null],
 
     /*
     |--------------------------------------------------------------------------
