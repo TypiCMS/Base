2c3e413a19f67633ff190dc7b6d2393976d22510
Samuel De Backer <sdebacker@gmail.com>
2016-04-02 09:56:06 +0200

    can route middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,6 +67,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'authorization' => \TypiCMS\Modules\Core\Http\Middleware\Authorization::class,
         'registrationAllowed' => \TypiCMS\Modules\Core\Http\Middleware\RegistrationAllowed::class,
