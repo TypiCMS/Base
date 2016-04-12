cb587eabe8c1abd4a453cae6caf6c61df9458bb2
Samuel De Backer <sdebacker@gmail.com>
2016-04-12 16:38:26 +0200

    commented code removed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,7 +29,6 @@ class Kernel extends HttpKernel
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            // \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
         ],
