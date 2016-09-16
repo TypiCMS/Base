521699d2626dd336f73b347f8d754cd27f4a39c6
Samuel De Backer <sdebacker@gmail.com>
2016-09-16 17:17:53 +0200

    SubstituteBindings middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,6 +32,7 @@ class Kernel extends HttpKernel
             // \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
         'public' => [
