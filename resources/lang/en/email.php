399c22b5dbf0c848226bf9b2545a8095e8c93ce3
Samuel De Backer <sdebacker@gmail.com>
2016-09-29 15:39:12 +0200

    Email notification translations

---
+++ resources/lang/en/email.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Regards' => 'Regards',
+    'If you’re having trouble clicking the ":actionText" button' => 'If you’re having trouble clicking the “:actionText” button',
+    'copy and paste the URL below into your web browser:' => 'copy and paste the URL below into your web browser:',
+    'All rights reserved' => 'All rights reserved',
+];
---
+++ resources/lang/fr/email.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Regards' => 'Cordialement',
+    'If you’re having trouble clicking the ":actionText" button' => 'Si vous ne pouvez pas cliquer sur le bouton « :actionText »',
+    'copy and paste the URL below into your web browser:' => 'copiez et collez l’adresse suivante dans votre navigateur :',
+    'All rights reserved' => 'Tous droits réservés',
+];
