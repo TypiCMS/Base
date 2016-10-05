02660172f1d89f328033b2cf01c4683747cb359a
Samuel De Backer <sdebacker@gmail.com>
2016-10-05 17:08:58 +0200

    status switches

--- resources/assets/typicms/components/list/directives/typiBtnStatus.js
+++ resources/assets/typicms/components/list/directives/typiBtnStatus.js
@@ -2,12 +2,13 @@ angular.module('typicms').directive('typiBtnStatus', function() {
     return {
         scope: {
             model: '=',
+            locale: '=locale',
             action: '&'
         },
         template: '<div class="btn btn-xs btn-link" ng-click="action()">' +
-                '<span class="fa switch" ng-class="model.status[TypiCMS.content_locale] == \'1\' ? \'fa-toggle-on\' : \'fa-toggle-off\'"></span>' +
-                '<span class="sr-only" ng-show="model.status[TypiCMS.content_locale] == \'1\'" translate>Online</span>' +
-                '<span class="sr-only" ng-hide="model.status[TypiCMS.content_locale] == \'0\'" translate>Offline</span>' +
+                '<span class="fa switch" ng-class="model.status[locale] == \'1\' ? \'fa-toggle-on\' : \'fa-toggle-off\'"></span>' +
+                '<span class="sr-only" ng-show="model.status[locale] == \'1\'" translate>Online</span>' +
+                '<span class="sr-only" ng-hide="model.status[locale] == \'0\'" translate>Offline</span>' +
             '</div>'
     };
 });
