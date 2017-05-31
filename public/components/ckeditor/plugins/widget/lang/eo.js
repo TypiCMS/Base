4f6c38acd1dcb3921b793571442e3e8dfcefd23c
Samuel De Backer <sdebacker@gmail.com>
2017-05-31 11:08:00 +0200

    ckeditor files

--- public/components/ckeditor/plugins/widget/lang/eo.js
+++ public/components/ckeditor/plugins/widget/lang/eo.js
@@ -2,4 +2,4 @@
  Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang("widget","eo",{move:"klaki kaj treni por movi",label:"%1 widget"});
+CKEDITOR.plugins.setLang("widget","eo",{move:"klaki kaj treni por movi",label:"%1 fenestraĵo"});
