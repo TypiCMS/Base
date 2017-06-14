fff2840cdd9a40ec48a3e43d21a088ba791ef027
Samuel De Backer <sdebacker@gmail.com>
2017-06-14 12:49:35 +0200

    kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,8 +14,6 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Illuminate\Session\Middleware\StartSession::class,
-        \TypiCMS\Modules\Core\Http\Middleware\SetLocale::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -32,10 +30,12 @@ class Kernel extends HttpKernel
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            // \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \TypiCMS\Modules\Core\Http\Middleware\SetLocale::class,
         ],
 
         'public' => [
@@ -46,7 +46,6 @@ class Kernel extends HttpKernel
         'admin' => [
             'web',
             'auth',
-            'authorization',
             \TypiCMS\Modules\Core\Http\Middleware\AdminLocale::class,
             \TypiCMS\Modules\Core\Http\Middleware\JavaScriptData::class,
             \TypiCMS\Modules\Core\Http\Middleware\UserPrefs::class,
@@ -72,7 +71,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'authorization' => \TypiCMS\Modules\Core\Http\Middleware\Authorization::class,
         'registrationAllowed' => \TypiCMS\Modules\Core\Http\Middleware\RegistrationAllowed::class,
     ];
 }
