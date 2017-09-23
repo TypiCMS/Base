2f2fb4beec7772a2158b627f90f6349260ced89f
Samuel De Backer <sdebacker@gmail.com>
2017-09-23 13:08:49 +0200

    Filesystem driver for uploads

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,13 @@ return [
             'visibility' => 'public',
         ],
 
+        'uploads' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/uploads'),
+            'url' => env('APP_URL').'/uploads',
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_KEY'),
