bca55b714207b33487d6685d1250de0aa4252301
Samuel De Backer <sdebacker@gmail.com>
2017-09-27 21:13:50 +0200

    uploads filesystem removed.

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,13 +55,6 @@ return [
             'visibility' => 'public',
         ],
 
-        'uploads' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public/uploads'),
-            'url' => env('APP_URL').'/uploads',
-            'visibility' => 'public',
-        ],
-
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_KEY'),
