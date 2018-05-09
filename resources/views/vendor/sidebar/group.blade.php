3415c36ba105744b6aeeb66f4774447021fca374
Samuel De Backer <sdebacker@gmail.com>
2018-05-09 19:32:47 +0200

    sidebar view

--- resources/views/vendor/sidebar/group.blade.php
+++ resources/views/vendor/sidebar/group.blade.php
@@ -4,7 +4,7 @@
             <div>{{ $group->name }}</div>
         </a>
     @endif
-    <ul class="nav nav-sidebar panel-collapse collapse @if(! Config::get('typicms.user.menus_'.$group->id.'_collapsed'))in @endif" id="{{ $group->id }}">
+    <ul class="nav-sidebar panel-collapse collapse @if(! Config::get('typicms.user.menus_'.$group->id.'_collapsed'))show @endif" id="{{ $group->id }}">
         @foreach($group->getItems() as $item)
             {!! $item->render() !!}
         @endforeach
