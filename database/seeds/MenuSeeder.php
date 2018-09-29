776da1a391bd647568cf6be333bfcfd24a45bcf7
Samuel De Backer <sdebacker@gmail.com>
2018-09-29 12:11:37 +0200

    Seeders cleaned

--- database/seeds/MenuSeeder.php
+++ database/seeds/MenuSeeder.php
@@ -7,9 +7,9 @@ class MenuSeeder extends Seeder
     public function run()
     {
         $typi_menus = [
-            ['id' => '1', 'name' => 'main', 'status' => '{"fr":"1","en":"1","nl":"1"}', 'class' => '', 'created_at' => '2013-09-03 22:05:21', 'updated_at' => '2014-02-17 16:00:00'],
-            ['id' => '2', 'name' => 'footer', 'status' => '{"fr":"1","en":"1","nl":"1"}', 'class' => '', 'created_at' => '2013-09-03 22:05:42', 'updated_at' => '2014-02-17 16:00:00'],
-            ['id' => '3', 'name' => 'social', 'status' => '{"fr":"1","en":"1","nl":"1"}', 'class' => '', 'created_at' => '2014-02-04 18:27:18', 'updated_at' => '2014-02-17 16:00:00'],
+            ['id' => '1', 'name' => 'main', 'status' => '{"fr":"1","en":"1","nl":"1"}', 'created_at' => '2013-09-03 22:05:21', 'updated_at' => '2014-02-17 16:00:00'],
+            ['id' => '2', 'name' => 'footer', 'status' => '{"fr":"1","en":"1","nl":"1"}', 'created_at' => '2013-09-03 22:05:42', 'updated_at' => '2014-02-17 16:00:00'],
+            ['id' => '3', 'name' => 'social', 'status' => '{"fr":"1","en":"1","nl":"1"}', 'created_at' => '2014-02-04 18:27:18', 'updated_at' => '2014-02-17 16:00:00'],
         ];
 
         DB::table('menus')->insert($typi_menus);
