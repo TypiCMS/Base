cd6bb53945d28a59d7601d5b6bb0787f3172c30b
Samuel De Backer <sdebacker@gmail.com>
2019-07-05 21:11:39 +0200

    sitemap config

--- config/sitemap.php
+++ config/sitemap.php
@@ -3,12 +3,12 @@
 /* Simple configuration file for Laravel Sitemap package */
 return [
     'use_cache' => false,
-    'cache_key' => 'laravel-sitemap.'.config('app.url'),
+    'cache_key' => 'sitemap.'.config('app.url'),
     'cache_duration' => 3600,
     'escaping' => true,
     'use_limit_size' => false,
     'max_size' => null,
-    'use_styles' => true,
+    'use_styles' => false,
     'styles_location' => '/vendor/sitemap/styles/',
     'use_gzip' => false
 ];
