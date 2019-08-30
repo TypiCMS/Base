f268a96d7d42eb5ff64d8fcddf7f85cc254aa028
Samuel De Backer <sdebacker@gmail.com>
2019-08-30 18:12:09 +0200

    facade/ignition

---
+++ config/ignition.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Editor
+    |--------------------------------------------------------------------------
+    |
+    | Here you can specify the editor that should be opened when clicking
+    | code links.
+    |
+    | Possible values are 'phpstorm', 'vscode', 'sublime' and 'atom'.
+    */
+
+    'editor' => 'sublime',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Theme
+    |--------------------------------------------------------------------------
+    |
+    | Specify which theme should be used. You can choose between 'light', 'dark' and 'auto'.
+    |
+    */
+
+    'theme' => 'light',
+];
