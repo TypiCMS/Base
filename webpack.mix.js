5668da687e8eef5d3e0db92e4f1944255dd5f5ab
Samuel De Backer <sdebacker@gmail.com>
2019-12-04 14:55:56 +0100

    ckeditor directory is now ckeditor4

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,16 +20,16 @@ mix.copy('node_modules/font-awesome/fonts', 'public/fonts');
 /**
  * Copy CKEditor main files
  */
-mix.copy('node_modules/ckeditor4/ckeditor.js', 'public/components/ckeditor/ckeditor.js');
-mix.copy('node_modules/ckeditor4/contents.css', 'public/components/ckeditor/contents.css');
+mix.copy('node_modules/ckeditor4/ckeditor.js', 'public/components/ckeditor4/ckeditor.js');
+mix.copy('node_modules/ckeditor4/contents.css', 'public/components/ckeditor4/contents.css');
 
 /**
  * Copy CKEditor lang files
  */
-mix.copy('node_modules/ckeditor4/lang/fr.js', 'public/components/ckeditor/lang/fr.js');
-mix.copy('node_modules/ckeditor4/lang/es.js', 'public/components/ckeditor/lang/es.js');
-mix.copy('node_modules/ckeditor4/lang/nl.js', 'public/components/ckeditor/lang/nl.js');
-mix.copy('node_modules/ckeditor4/lang/en.js', 'public/components/ckeditor/lang/en.js');
+mix.copy('node_modules/ckeditor4/lang/fr.js', 'public/components/ckeditor4/lang/fr.js');
+mix.copy('node_modules/ckeditor4/lang/es.js', 'public/components/ckeditor4/lang/es.js');
+mix.copy('node_modules/ckeditor4/lang/nl.js', 'public/components/ckeditor4/lang/nl.js');
+mix.copy('node_modules/ckeditor4/lang/en.js', 'public/components/ckeditor4/lang/en.js');
 
 /**
  * Copy CKEditor plugins files
@@ -55,13 +55,13 @@ var plugins = [
     'wsc',
 ];
 plugins.forEach(function(plugin) {
-    mix.copy('node_modules/ckeditor4/plugins/' + plugin, 'public/components/ckeditor/plugins/' + plugin);
+    mix.copy('node_modules/ckeditor4/plugins/' + plugin, 'public/components/ckeditor4/plugins/' + plugin);
 });
 
 /**
  * Copy CKEditor skins files
  */
-mix.copy('node_modules/ckeditor4/skins', 'public/components/ckeditor/skins');
+mix.copy('node_modules/ckeditor4/skins', 'public/components/ckeditor4/skins');
 
 /**
  * Versioning process
