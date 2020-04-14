e694f2900e705e925181505f9d1d09beab4822e8
Samuel De Backer <sdebacker@gmail.com>
2020-04-14 10:39:13 +0200

    Seeder

--- database/seeds/MenuSeeder.php
+++ database/seeds/MenuSeeder.php
@@ -8,9 +8,10 @@ class MenuSeeder extends Seeder
     public function run()
     {
         $typi_menus = [
-            ['id' => 1, 'name' => 'main', 'status' => '{"fr":"1","en":"1","nl":"1"}', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['id' => 2, 'name' => 'footer', 'status' => '{"fr":"1","en":"1","nl":"1"}', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['id' => 3, 'name' => 'social', 'status' => '{"fr":"1","en":"1","nl":"1"}', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['id' => 1, 'image_id' => null, 'name' => 'main', 'class' => null, 'status' => '{"fr":"1","en":"1","nl":"1"}', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['id' => 2, 'image_id' => null, 'name' => 'footer', 'class' => null, 'status' => '{"fr":"1","en":"1","nl":"1"}', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['id' => 3, 'image_id' => null, 'name' => 'social', 'class' => null, 'status' => '{"fr":"1","en":"1","nl":"1"}', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['id' => 4, 'image_id' => null, 'name' => 'legal', 'class' => null, 'status' => '{"fr": 1, "en": 1, "nl": 1}', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
         ];
 
         DB::table('menus')->insert($typi_menus);
