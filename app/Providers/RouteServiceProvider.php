018a051dabba4a6ace8f09927b3a7c8c6786e797
Samuel De Backer <sdebacker@gmail.com>
2021-09-08 16:28:00 +0200

    Path to the "home" route.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
 
     /**
      * If specified, this namespace is automatically applied to your controller routes.
