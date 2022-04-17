4ad60b5cff63b0ca2316ed4c84cdab3ff381a219
Samuel De Backer <sdebacker@gmail.com>
2022-04-17 15:37:20 +0200

    config files

--- config/flare.php
+++ config/flare.php
@@ -1,5 +1,17 @@
 <?php
 
+use Spatie\FlareClient\FlareMiddleware\AddGitInformation;
+use Spatie\FlareClient\FlareMiddleware\RemoveRequestIp;
+use Spatie\FlareClient\FlareMiddleware\CensorRequestBodyFields;
+use Spatie\FlareClient\FlareMiddleware\CensorRequestHeaders;
+use Spatie\LaravelIgnition\FlareMiddleware\AddDumps;
+use Spatie\LaravelIgnition\FlareMiddleware\AddEnvironmentInformation;
+use Spatie\LaravelIgnition\FlareMiddleware\AddExceptionInformation;
+use Spatie\LaravelIgnition\FlareMiddleware\AddJobs;
+use Spatie\LaravelIgnition\FlareMiddleware\AddLogs;
+use Spatie\LaravelIgnition\FlareMiddleware\AddQueries;
+use Spatie\LaravelIgnition\FlareMiddleware\AddNotifierName;
+
 return [
     /*
     |
@@ -7,8 +19,7 @@ return [
     | Flare API key
     |--------------------------------------------------------------------------
     |
-    | If you want to send your errors to the Flare service, you can specify
-    | the API key of your project here.
+    | Specify Flare's API key below to enable error reporting to the service.
     |
     | More info: https://flareapp.io/docs/general/projects
     |
@@ -18,19 +29,51 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Reporting options
+    | Middleware
     |--------------------------------------------------------------------------
     |
-    | These options determine which information is being transmitted
-    | to Flare.
+    | These middleware will modify the contents of the report sent to Flare.
     |
     */
 
-    'reporting' => [
-        'anonymize_ips' => true,
-        'collect_git_information' => true,
-        'report_queries' => true,
-        'report_query_bindings' => true,
-        'report_view_data' => true,
+    'flare_middleware' => [
+        RemoveRequestIp::class,
+        AddGitInformation::class,
+        AddNotifierName::class,
+        AddEnvironmentInformation::class,
+        AddExceptionInformation::class,
+        AddDumps::class,
+        AddLogs::class => [
+            'maximum_number_of_collected_logs' => 200,
+        ],
+        AddQueries::class => [
+            'maximum_number_of_collected_queries' => 200,
+            'report_query_bindings' => true,
+        ],
+        AddJobs::class => [
+            'max_chained_job_reporting_depth' => 5,
+        ],
+        CensorRequestBodyFields::class => [
+            'censor_fields' => [
+                'password',
+            ],
+        ],
+        CensorRequestHeaders::class => [
+            'headers' => [
+                'API-KEY'
+            ]
+        ]
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Reporting log statements
+    |--------------------------------------------------------------------------
+    |
+    | If this setting is `false` log statements won't be sent as events to Flare,
+    | no matter which error level you specified in the Flare log channel.
+    |
+    */
+
+    'send_logs_as_events' => true,
 ];
--- config/ignition.php
+++ config/ignition.php
@@ -1,16 +1,35 @@
 <?php
 
-return [
+use Spatie\Ignition\Solutions\SolutionProviders\BadMethodCallSolutionProvider;
+use Spatie\Ignition\Solutions\SolutionProviders\MergeConflictSolutionProvider;
+use Spatie\Ignition\Solutions\SolutionProviders\UndefinedPropertySolutionProvider;
+use Spatie\LaravelIgnition\Solutions\SolutionProviders\DefaultDbNameSolutionProvider;
+use Spatie\LaravelIgnition\Solutions\SolutionProviders\GenericLaravelExceptionSolutionProvider;
+use Spatie\LaravelIgnition\Solutions\SolutionProviders\IncorrectValetDbCredentialsSolutionProvider;
+use Spatie\LaravelIgnition\Solutions\SolutionProviders\InvalidRouteActionSolutionProvider;
+use Spatie\LaravelIgnition\Solutions\SolutionProviders\MissingAppKeySolutionProvider;
+use Spatie\LaravelIgnition\Solutions\SolutionProviders\MissingColumnSolutionProvider;
+use Spatie\LaravelIgnition\Solutions\SolutionProviders\MissingImportSolutionProvider;
+use Spatie\LaravelIgnition\Solutions\SolutionProviders\MissingLivewireComponentSolutionProvider;
+use Spatie\LaravelIgnition\Solutions\SolutionProviders\MissingMixManifestSolutionProvider;
+use Spatie\LaravelIgnition\Solutions\SolutionProviders\RunningLaravelDuskInProductionProvider;
+use Spatie\LaravelIgnition\Solutions\SolutionProviders\TableNotFoundSolutionProvider;
+use Spatie\LaravelIgnition\Solutions\SolutionProviders\UndefinedViewVariableSolutionProvider;
+use Spatie\LaravelIgnition\Solutions\SolutionProviders\UnknownValidationSolutionProvider;
+use Spatie\LaravelIgnition\Solutions\SolutionProviders\ViewNotFoundSolutionProvider;
 
+return [
     /*
     |--------------------------------------------------------------------------
     | Editor
     |--------------------------------------------------------------------------
     |
-    | Here you can specify the editor that should be opened when clicking
-    | code links.
+    | Choose your preferred editor to use when clicking any edit button.
     |
-    | Possible values are 'phpstorm', 'vscode', 'vscode-insiders', 'sublime' and 'atom'.
+    | Supported: "phpstorm", "vscode", "vscode-insiders", "textmate", "emacs",
+    |            "sublime", "atom", "nova", "macvim", "idea", "netbeans",
+    |            "xdebug"
+    |
     */
 
     'editor' => env('IGNITION_EDITOR', 'sublime'),
@@ -20,20 +39,23 @@ return [
     | Theme
     |--------------------------------------------------------------------------
     |
-    | Specify which theme should be used. You can choose between 'light', 'dark' and 'auto'.
+    | Here you may specify which theme Ignition should use.
+    |
+    | Supported: "light", "dark", "auto"
     |
     */
 
-    'theme' => env('IGNITION_THEME', 'light'),
+    'theme' => env('IGNITION_THEME', 'auto'),
 
     /*
     |--------------------------------------------------------------------------
     | Sharing
     |--------------------------------------------------------------------------
     |
-    | Ignition allows you to manually share your local errors with colleagues or people around the world.
-    | Sharing errors is completely free and does not require an account on Flare.
-    | If you do not want to have the ability to share your local errors, you can disable it here.
+    | You can share local errors with colleagues or others around the world.
+    | Sharing is completely free and doesn't require an account on Flare.
+    |
+    | If necessary, you can completely disable sharing below.
     |
     */
 
@@ -41,31 +63,81 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Ignore solution providers
+    | Register Ignition commands
     |--------------------------------------------------------------------------
     |
-    | Here you can specify the fully qualified class names of solution providers
-    | that should not be used.
+    | Ignition comes with an additional make command that lets you create
+    | new solution classes more easily. To keep your default Laravel
+    | installation clean, this command is not registered by default.
+    |
+    | You can enable the command registration below.
     |
     */
 
-    'ignored_solution_providers' => [
-        //
+    'register_commands' => env('REGISTER_IGNITION_COMMANDS', false),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Solution Providers
+    |--------------------------------------------------------------------------
+    |
+    | You may specify a list of solution providers (as fully qualified class
+    | names) that shouldn't be loaded. Ignition will ignore these classes
+    | and possible solutions provided by them will never be displayed.
+    |
+    */
+
+    'solution_providers' => [
+        // from spatie/ignition
+        BadMethodCallSolutionProvider::class,
+        MergeConflictSolutionProvider::class,
+        UndefinedPropertySolutionProvider::class,
+
+        // from spatie/laravel-ignition
+        IncorrectValetDbCredentialsSolutionProvider::class,
+        MissingAppKeySolutionProvider::class,
+        DefaultDbNameSolutionProvider::class,
+        TableNotFoundSolutionProvider::class,
+        MissingImportSolutionProvider::class,
+        InvalidRouteActionSolutionProvider::class,
+        ViewNotFoundSolutionProvider::class,
+        RunningLaravelDuskInProductionProvider::class,
+        MissingColumnSolutionProvider::class,
+        UnknownValidationSolutionProvider::class,
+        MissingMixManifestSolutionProvider::class,
+        MissingLivewireComponentSolutionProvider::class,
+        UndefinedViewVariableSolutionProvider::class,
+        GenericLaravelExceptionSolutionProvider::class,
     ],
 
     /*
     |--------------------------------------------------------------------------
-    | Runnable solutions
+    | Ignored Solution Providers
     |--------------------------------------------------------------------------
     |
-    | Some of the solutions that Ignition presents to you are runnable and can perform various
-    | tasks. By default this is only enabled if your application is running in debug mode.
-    | If you would rather disable this feature completely, you can do so using the
-    | enable_solution_execution configuration key.
+    | You may specify a list of solution providers (as fully qualified class
+    | names) that shouldn't be loaded. Ignition will ignore these classes
+    | and possible solutions provided by them will never be displayed.
     |
     */
 
-    'enable_runnable_solutions' => env('IGNITION_ENABLE_RUNNABLE_SOLUTIONS', null),
+    'ignored_solution_providers' => [
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Runnable Solutions
+    |--------------------------------------------------------------------------
+    |
+    | Some solutions that Ignition displays are runnable and can perform
+    | various tasks. By default, runnable solutions are enabled when your app
+    | has debug mode enabled. You may also fully disable this feature.
+    |
+    | Default: env('IGNITION_ENABLE_RUNNABLE_SOLUTIONS', env('APP_DEBUG', false))
+    |
+    */
+
+    'enable_runnable_solutions' => env('IGNITION_ENABLE_RUNNABLE_SOLUTIONS', env('APP_DEBUG', false)),
 
     /*
     |--------------------------------------------------------------------------
@@ -74,19 +146,59 @@ return [
     |
     | If you are using a remote dev server, like Laravel Homestead, Docker, or
     | even a remote VPS, it will be necessary to specify your path mapping.
-    | Leaving either one, or both of these, empty or null will not trigger the
-    | remote URL changes, and will treat your editor links as local files.
     |
-    | 'remote_sites_path' is the full base path of your sites or projects inside
-    |                     homestead, Docker, or remote dev servers, for example
-    |                     `/home/vagrant/Code`.
-    | 'local_sites_path'  is the full base path of your sites or projects on your
-    |                     local computer that your IDE or editor is running on,
-    |                     for example `/Users/<name>/Code` or
-    |                     `C:\Users\<name>\Documents\Code`.
+    | Leaving one, or both of these, empty or null will not trigger the remote
+    | URL changes and Ignition will treat your editor links as local files.
+    |
+    | "remote_sites_path" is an absolute base path for your sites or projects
+    | in Homestead, Vagrant, Docker, or another remote development server.
+    |
+    | Example value: "/home/vagrant/Code"
+    |
+    | "local_sites_path" is an absolute base path for your sites or projects
+    | on your local computer where your IDE or code editor is running on.
+    |
+    | Example values: "/Users/<name>/Code", "C:\Users\<name>\Documents\Code"
+    |
     */
 
-    'remote_sites_path' => env('IGNITION_REMOTE_SITES_PATH', ''),
+    'remote_sites_path' => env('IGNITION_REMOTE_SITES_PATH', base_path()),
     'local_sites_path' => env('IGNITION_LOCAL_SITES_PATH', ''),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Housekeeping Endpoint Prefix
+    |--------------------------------------------------------------------------
+    |
+    | Ignition registers a couple of routes when it is enabled. Below you may
+    | specify a route prefix that will be used to host all internal links.
+    |
+    */
+
+    'housekeeping_endpoint_prefix' => '_ignition',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Settings File
+    |--------------------------------------------------------------------------
+    |
+    | Ignition allows you to save your settings to a specific global file.
+    |
+    | If no path is specified, a file with settings will be saved to the user's
+    | home directory. The directory depends on the OS and its settings but it's
+    | typically `~/.ignition.json`. In this case, the settings will be applied
+    | to all of your projects where Ignition is used and the path is not
+    | specified.
+    |
+    | However, if you want to store your settings on a project basis, or you
+    | want to keep them in another directory, you can specify a path where
+    | the settings file will be saved. The path should be an existing directory
+    | with correct write access.
+    | For example, create a new `ignition` folder in the storage directory and
+    | use `storage_path('ignition')` as the `settings_file_path`.
+    |
+    | Default value: '' (empty string)
+    */
+
+    'settings_file_path' => '',
 ];
