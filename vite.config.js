223ccffeb121d26ec63abea8fda69772c25a8213
Samuel De Backer <sdebacker@gmail.com>
2023-04-06 18:18:18 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,30 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue2';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm.js',
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/scss/public.scss', 'resources/js/public.js'],
+            input: [
+                'resources/scss/public.scss',
+                'resources/scss/admin.scss',
+                'resources/js/public.js',
+                'resources/js/admin.js',
+            ],
             refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    // base: null,
+                    // includeAbsolute: false,
+                },
+            },
         }),
     ],
 });
