0296c0a6f964712b05f74c33d138be6a34c30576
Samuel De Backer <sdebacker@gmail.com>
2023-04-11 22:44:09 +0200

    Webpack and mix removed

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue2';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     resolve: {
@@ -25,6 +26,57 @@ export default defineConfig({
                     // includeAbsolute: false,
                 },
             },
+        }),
+        viteStaticCopy({
+            targets: [
+                // Main CKEditor files
+                {
+                    src: ['node_modules/ckeditor4/ckeditor.js', 'node_modules/ckeditor4/contents.css'],
+                    dest: '../../public/components/ckeditor4/',
+                },
+                // CKEditor skins
+                {
+                    src: 'node_modules/ckeditor4/skins',
+                    dest: '../../public/components/ckeditor4/skins/',
+                },
+                // CKEditor langs
+                {
+                    src: [
+                        'node_modules/ckeditor4/lang/fr.js',
+                        'node_modules/ckeditor4/lang/nl.js',
+                        'node_modules/ckeditor4/lang/en.js',
+                        'node_modules/ckeditor4/lang/es.js',
+                    ],
+                    dest: '../../public/components/ckeditor4/lang/',
+                },
+                // CKEditor plugins
+                {
+                    src: [
+                        'node_modules/ckeditor4/plugins/dialog',
+                        'node_modules/ckeditor4/plugins/dialogadvtab',
+                        'node_modules/ckeditor4/plugins/div',
+                        'node_modules/ckeditor4/plugins/embed',
+                        'node_modules/ckeditor4/plugins/embedbase',
+                        'node_modules/ckeditor4/plugins/image',
+                        'node_modules/ckeditor4/plugins/image2',
+                        'node_modules/ckeditor4/plugins/justify',
+                        'node_modules/ckeditor4/plugins/link',
+                        'node_modules/ckeditor4/plugins/magicline',
+                        'node_modules/ckeditor4/plugins/panelbutton',
+                        'node_modules/ckeditor4/plugins/pastefromgdocs',
+                        'node_modules/ckeditor4/plugins/pastefromword',
+                        'node_modules/ckeditor4/plugins/pastetools',
+                        'node_modules/ckeditor4/plugins/scayt',
+                        'node_modules/ckeditor4/plugins/showblocks',
+                        'node_modules/ckeditor4/plugins/specialchar',
+                        'node_modules/ckeditor4/plugins/table',
+                        'node_modules/ckeditor4/plugins/tableselection',
+                        'node_modules/ckeditor4/plugins/tabletools',
+                        'node_modules/ckeditor4/plugins/widget',
+                    ],
+                    dest: '../../public/components/ckeditor4/plugins/',
+                },
+            ],
         }),
     ],
 });
