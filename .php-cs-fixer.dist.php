c2de33629d987cc4e5beaaf9721e23fd3ffef3ba
Samuel De Backer <sdebacker@gmail.com>
2023-04-17 11:39:24 +0200

    php-cs-fixer and pint config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,15 +2,17 @@
 
 $config = new PhpCsFixer\Config();
 $config->setRules([
-    '@PSR12' => true,
     '@PhpCsFixer' => true,
+    '@PHP80Migration' => true,
     'mb_str_functions' => true,
     'multiline_whitespace_before_semicolons' => ['strategy' => 'no_multi_line'],
     'modernize_types_casting' => true,
     'return_assignment' => false,
-    'ordered_class_elements' => false,
+    'ordered_class_elements' => ['order' => ['use_trait']],
     'single_line_comment_style' => false,
     'yoda_style' => false,
+    'global_namespace_import' => true,
+    'concat_space' => ['spacing' => 'one'],
 ])
     ->setRiskyAllowed(true)
     ->setUsingCache(false);
