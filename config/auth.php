6a12326a000b60b70a772a541b7bc9deb922a948
Samuel De Backer <sdebacker@gmail.com>
2023-04-18 23:08:01 +0200

    password_resets table

--- config/auth.php
+++ config/auth.php
@@ -97,7 +97,7 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
-            'table' => 'password_reset_tokens',
+            'table' => 'password_resets',
             'expire' => 60,
             'throttle' => 60,
         ],
