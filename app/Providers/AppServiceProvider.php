62da63f604191ce2231dec9a19ff31707734600c
Samuel De Backer <sdebacker@gmail.com>
2023-04-18 14:03:49 +0200

    Model::shouldBeStrict(false);

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::shouldBeStrict(false); // Strict mode is currently not supported by TypiCMS.
     }
 }
