6df917c76d58f59a834d94c8ca1c56f2613085b6
Samuel De Backer <sdebacker@gmail.com>
2023-11-23 14:47:24 +0100

    PoweredByHeader

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,6 +44,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
+            \TypiCMS\Modules\Core\Http\Middleware\PoweredByHeader::class,
             \TypiCMS\Modules\Core\Http\Middleware\Impersonate::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \TypiCMS\Modules\Core\Http\Middleware\SetNavbarLocale::class,
