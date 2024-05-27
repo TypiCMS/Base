93e1c33c3d678bf8d8de4f1afd0d373c43636e7f
Samuel De Backer <sdebacker@gmail.com>
2024-05-27 18:59:45 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,7 +30,6 @@ use TypiCMS\Modules\Core\Providers\PagesRoutesServiceProvider;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
         then: function (Application $app) {
