74798bb059befa888aaa157fcac71ce01f2e1504
Samuel De Backer <sdebacker@gmail.com>
2024-10-03 09:42:52 +0200

    Update services.php

--- config/services.php
+++ config/services.php
@@ -14,10 +14,10 @@ return [
     */
 
     'mailgun' => [
+        'transport' => 'mailgun',
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-        'scheme' => 'https',
     ],
 
     'postmark' => [
