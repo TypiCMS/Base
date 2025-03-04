7042d421a3d3fd1105665931fdcf90599b2b05d6
Samuel De Backer <sdebacker@gmail.com>
2025-03-04 15:08:14 +0100

    Laravel 12

--- config/session.php
+++ config/session.php
@@ -31,7 +31,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => (int) env('SESSION_LIFETIME', 120),
 
     'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
 
--- tests/TestCase.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Tests;
-
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-
-abstract class TestCase extends BaseTestCase {}
