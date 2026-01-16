3fa06982932c03e1987905d469d3af24650658d2
Samuel De Backer <sdebacker@gmail.com>
2026-01-16 22:38:06 +0100

    Switch to latest sass-embedded

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,19 @@ export default defineConfig({
     build: {
         chunkSizeWarningLimit: 2000,
     },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler',
+                silenceDeprecations: ['import', 'global-builtin', 'color-functions', 'if-function'],
+            },
+        },
+        devSourcemap: true,
+    },
     plugins: [
         laravel({
             input: ['resources/scss/public.scss', 'resources/scss/admin.scss', 'resources/js/public.js', 'resources/js/admin.js', 'resources/js/admin/theme-switcher.ts'],
-            refresh: true,
+            refresh: ['resources/views/**'],
         }),
         vue({
             template: {
