535b5b2056ad786c34e6fe83652627f27c1a6d2d
Samuel De Backer <sdebacker@gmail.com>
2026-02-21 14:21:07 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,93 +1,99 @@
+import js from '@eslint/js';
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
-import pluginImport from 'eslint-plugin-import';
+import pluginImport from 'eslint-plugin-import-x';
 import pluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
 import vueParser from 'vue-eslint-parser';
 
-export default [
-  {
-    ignores: [
-        '**/build/**',
-        '**/public/build/**',
-        '**/node_modules/**',
-        '**/vendor/**',
-        'resources/js/admin.js',
+const importOrderRules = {
+    'import/order': [
+        'error',
+        {
+            groups: [
+                'builtin',
+                'external',
+                'internal',
+                'parent',
+                'sibling',
+                'index',
+                'object',
+                'type',
+            ],
+            'newlines-between': 'always',
+            alphabetize: {
+                order: 'asc',
+                caseInsensitive: true,
+            },
+        },
     ],
-  },
-  // JavaScript/TypeScript files
-  {
-    files: ['**/*.{js,ts}'],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
+};
+
+export default [
+    {
+        ignores: [
+            '**/build/**',
+            '**/public/build/**',
+            '**/node_modules/**',
+            '**/vendor/**',
+            'resources/js/admin.js',
+            '**/*.d.ts',
+        ],
     },
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-      'import': pluginImport,
+    js.configs.recommended,
+    // JavaScript/TypeScript files
+    {
+        files: ['**/*.{js,ts}'],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                TypiCMS: 'readonly',
+                alertify: 'readonly',
+                emitter: 'readonly',
+                setItemsHeight: 'readonly',
+                registerMasonry: 'readonly',
+                RequestInit: 'readonly',
+                NodeListOf: 'readonly',
+                ChildNode: 'readonly',
+            },
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': typescriptEslint,
+            import: pluginImport,
+        },
+        rules: importOrderRules,
     },
-    rules: {
-      'import/order': [
-        'error',
-        {
-          'groups': [
-            'builtin',
-            'external',
-            'internal',
-            'parent',
-            'sibling',
-            'index',
-            'object',
-            'type'
-          ],
-          'newlines-between': 'always',
-          'alphabetize': {
-            'order': 'asc',
-            'caseInsensitive': true
-          }
-        }
-      ],
+    // Vue files
+    ...pluginVue.configs['flat/essential'],
+    {
+        files: ['**/*.vue'],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                TypiCMS: 'readonly',
+                alertify: 'readonly',
+                emitter: 'readonly',
+            },
+            parser: vueParser,
+            parserOptions: {
+                parser: tsParser,
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': typescriptEslint,
+            import: pluginImport,
+        },
+        rules: {
+            ...importOrderRules,
+            'vue/attributes-order': 'error',
+            'vue/multi-word-component-names': 'off',
+        },
     },
-  },
-  // Vue files
-  {
-    files: ['**/*.vue'],
-    languageOptions: {
-      parser: vueParser,
-      parserOptions: {
-        parser: tsParser,
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      vue: pluginVue,
-      '@typescript-eslint': typescriptEslint,
-      'import': pluginImport,
-    },
-    rules: {
-      'import/order': [
-        'error',
-        {
-          'groups': [
-            'builtin',
-            'external',
-            'internal',
-            'parent',
-            'sibling',
-            'index',
-            'object',
-            'type'
-          ],
-          'newlines-between': 'always',
-          'alphabetize': {
-            'order': 'asc',
-            'caseInsensitive': true
-          }
-        }
-      ],
-    },
-  },
 ];
