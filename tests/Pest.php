ec005f9aa8f226826d628230cbdf957bf879fc3a
Samuel De Backer <sdebacker@gmail.com>
2026-03-20 15:47:43 +0100

    tests config

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 /*
@@ -14,8 +15,13 @@ use Tests\TestCase;
 */
 
 pest()->extend(TestCase::class)
+    ->use(DatabaseTransactions::class)
  // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Feature');
+
+pest()
+    ->extend(TestCase::class)
+    ->in('Unit');
 
 /*
 |--------------------------------------------------------------------------
