21f922cd53689cd87b624ff4072237fdcea2312a
Samuel De Backer <sdebacker@gmail.com>
2026-03-30 20:08:17 +0200

    Update providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,25 +1,8 @@
 <?php
 
 use App\Providers\AppServiceProvider;
-use TypiCMS\LaravelTranslatableBootForms\TranslatableBootFormsServiceProvider;
-use TypiCMS\Modules\Core\Providers\ModuleServiceProvider;
-use TypiCMS\Modules\Core\Providers\TranslationsServiceProvider;
-use TypiCMS\Modules\Sidebar\SidebarServiceProvider;
-use Typidesign\Translations\ArtisanTranslationsServiceProvider;
 
 return [
-    // Package Service Providers.
-    SidebarServiceProvider::class,
-    ArtisanTranslationsServiceProvider::class,
-    TranslatableBootFormsServiceProvider::class,
-    TranslationsServiceProvider::class,
-
-    // TypiCMS Core Service Provider.
-    ModuleServiceProvider::class,
-
-    // TypiCMS Modules Service Providers.
-    // TypiCMS\Modules\News\Providers\ModuleServiceProvider::class,
-
     // Application Service Providers.
     AppServiceProvider::class,
 ];
