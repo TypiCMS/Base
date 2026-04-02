7f32d1f0ee93752e0d2f411697dff78e29f31fb4
Samuel De Backer <sdebacker@gmail.com>
2026-04-02 10:54:08 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,23 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
-use Spatie\ResponseCache\Facades\ResponseCache;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void {}
+    public function register(): void
+    {
+        //
+    }
 
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        Event::listen('eloquent.saved:*', fn () => ResponseCache::clear());
-        Event::listen('eloquent.deleted:*', fn () => ResponseCache::clear());
+        //
     }
 }
