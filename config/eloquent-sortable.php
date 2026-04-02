b414d3accc1a9500de3c9920b292b0a452e59a3c
Samuel De Backer <sdebacker@gmail.com>
2026-04-02 10:54:03 +0200

    Update eloquent-sortable.php

--- config/eloquent-sortable.php
+++ config/eloquent-sortable.php
@@ -8,7 +8,13 @@ return [
 
     /*
      * Define if the models should sort when creating.
-     * When true, the package will automatically assign the highest order number to a new mode
+     * When true, the package will automatically assign the highest order number to a new model
      */
     'sort_when_creating' => true,
+
+    /*
+     * Define if the timestamps should be ignored when sorting.
+     * When true, updated_at will not be updated when using setNewOrder
+     */
+    'ignore_timestamps' => false,
 ];
